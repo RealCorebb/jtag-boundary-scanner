41d0459f0080910d0b902654ba70abaacba15ef4
RealCorebb <coreoobb@qq.com>
2024-10-17 09:20:32 +0800

    test

--- lib_jtag_core/src/jtag_core.c
+++ lib_jtag_core/src/jtag_core.c
@@ -739,8 +739,8 @@ int jtagcore_push_and_pop_chain(jtag_core *jc, int mode)
 						if (d == (jc->nb_of_devices_in_chain - 1)) // Last device in chain ?
 						{
 							jc->io_functions.drv_TXRX_DATA(jc, jc->devices_list[d].out_boundary_scan, jc->devices_list[d].in_boundary_scan, bsdl->number_of_chainbits - 1);
-							/* buf_out[0] = jc->devices_list[d].out_boundary_scan[bsdl->number_of_chainbits - 1] | JTAG_STR_TMS;
-							jc->io_functions.drv_TXRX_DATA(jc, (unsigned char *)&buf_out, jc->devices_list[d].in_boundary_scan[bsdl->number_of_chainbits - 1], 1); */
+							buf_out[0] = jc->devices_list[d].out_boundary_scan[bsdl->number_of_chainbits - 1] | JTAG_STR_TMS;
+							jc->io_functions.drv_TXRX_DATA(jc, buf_out, jc->devices_list[d].in_boundary_scan[bsdl->number_of_chainbits - 1], 1);
 						}
 						else
 						{
